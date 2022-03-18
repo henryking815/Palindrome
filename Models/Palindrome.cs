e3e2167fef299c525298f74bd56cfb9f2f80eba5
henryking815 <henryking815.hk@gmail.com>
2022-03-17 19:25:29 -0500

    Initial commit

---
+++ Models/Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HKingPalindrome.Models
+{
+    public class Palindrome
+    {
+        [DisplayName("Enter a positive number to see if it is a Palindrome")]
+        public string Number { get; set; }
+
+        [DisplayName("Palindrome:")]
+        public bool IsPalindrome { get; set; }
+
+        public Palindrome()
+        {
+
+        }
+
+        public Palindrome(string num)
+        {
+            Number = num;
+        }
+
+        public bool IsInputPalindrome(string number)
+        {
+            for (int i = 0; i < number.Length / 2; i++) //only need to loop half the length
+                //compare first and last char, if != return false
+                if (number[i] != number[number.Length - 1 - i]) return false;
+            return true;
+        }
+    }
+}
